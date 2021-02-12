39063e8b645327a8ab817dcc39c33405a3acfa41
Tyler Cook <tcc@sandpolis.com>
2021-02-11 17:48:44 -1000

    Add root settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+//============================================================================//
+//                                                                            //
+//                         Copyright © 2015 Sandpolis                         //
+//                                                                            //
+//  This source file is subject to the terms of the Mozilla Public License    //
+//  version 2. You may not use this file except in compliance with the MPL    //
+//  as published by the Mozilla Foundation.                                   //
+//                                                                            //
+//============================================================================//
+
+rootProject.name = "com.sandpolis.client.lifegem"
+
+// Core modules
+include("module:com.sandpolis.core.client")
+include("module:com.sandpolis.core.clientagent")
+include("module:com.sandpolis.core.foundation")
+include("module:com.sandpolis.core.instance")
+include("module:com.sandpolis.core.net")
